99fc64caf0926e56d29019cadcc50b996f7c7f88
Aryan Sharma <aryansharma2k4@gmail.com>
2026-04-13 18:13:07 +0530

    touchpad: added three finger swipe

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,17 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #function fish_greeting
 #    # smth smth
 #end
+
+function y --description "Launch Yazi and adopt its working directory"
+    set -l tmp (mktemp -t yazi-cwd.XXXXXX)
+    command yazi $argv --cwd-file="$tmp"
+
+    if test -f "$tmp"
+        set -l cwd (command cat -- "$tmp")
+        if test -n "$cwd"; and test "$cwd" != "$PWD"
+            builtin cd -- "$cwd"
+        end
+    end
+
+    command rm -f -- "$tmp"
+end
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,30 +1,21 @@
+
 return {
   {
     "datsfilipe/vesper.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require('vesper').setup({
-        transparent = false, -- We want it solid black, not see-through
-        italics = {
-          comments = true,
-          keywords = true,
-          functions = true,
-          strings = true,
-        },
+      require("vesper").setup({
+        transparent = false,
       })
+
       vim.cmd.colorscheme("vesper")
 
-      -- Force OLED Black (#000000) on all background elements
-      local oled_groups = {
-        "Normal", "NormalNC", "SignColumn", "StatusLine", 
-        "StatusLineNC", "EndOfBuffer", "MsgArea", "NvimTreeNormal",
-        "TabLineFill", "Folded"
-      }
-      
-      for _, group in ipairs(oled_groups) do
-        vim.api.nvim_set_hl(0, group, { fg = "NONE", bg = "#000000" })
-      end
+      -- Force solid black background
+      vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+      vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
+      vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
+      vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#000000" })
     end
   }
 }
